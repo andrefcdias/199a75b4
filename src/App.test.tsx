29dd397f215cad4dddcf7652534d5157e368c8e4
André <39736248+andrefcdias@users.noreply.github.com>
2025-05-09 12:10:12 +0200

    remove unnecessary test setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from "../src/App";
+import App from "./App";
 
 it("has a main heading", () => {
   render(<App />);
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./tests/setup.ts",
+    environment: "jsdom"
   },
 });
